5a21d96be3d1749aaa8e2b6b2c18c7ad395b0935
kartik10-24 <kartik.mendu@mindinventory.com>
2025-01-13 17:29:21 +0530

    Created Config file

---
+++ config.ts
@@ -0,0 +1,32 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import { function_declaration } from "./functions";
+
+const API_KEY = "AIzaSyCHv9k3f7aW_cE_V7HqhVcDeXmLH_oO-HU";
+
+const genAI = new GoogleGenerativeAI(API_KEY);
+
+const GM = genAI.getGenerativeModel({
+  model: "gemini-1.5-flash",
+  tools: [
+    {
+      functionDeclarations: [
+        function_declaration.apply_for_leave,
+        function_declaration.apply_for_wfh,
+        function_declaration.book_meeting_room,
+        function_declaration.request_it_support,
+        function_declaration.see_leave_balance,
+      ],
+    },
+  ],
+});
+
+export const getModel = (previousChat: { role: string; content: string }[]) => {
+  if (!previousChat.length) return GM.startChat();
+
+  const history = previousChat.map(({ role, content }) => ({
+    role,
+    parts: [{ text: content }],
+  }));
+
+  return GM.startChat({ history });
+};
--- index.ts
+++ index.ts
@@ -1,41 +1,12 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
 import Express from "express";
 import cors from "cors";
-import { function_declaration, function_method } from "./functions";
+import { function_method } from "./functions";
+import { getModel } from "./config";
 
 const app = Express();
-const API_KEY = "Your Key";
 
 app.use(cors({ origin: "*" }));
 app.use(Express.json());
-
-const genAI = new GoogleGenerativeAI(API_KEY);
-
-const GM = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
-  tools: [
-    {
-      functionDeclarations: [
-        function_declaration.apply_for_leave,
-        function_declaration.apply_for_wfh,
-        function_declaration.book_meeting_room,
-        function_declaration.request_it_support,
-        function_declaration.see_leave_balance,
-      ],
-    },
-  ],
-});
-
-const getModel = (previousChat: { role: string; content: string }[]) => {
-  if (!previousChat.length) return GM.startChat();
-
-  const history = previousChat.map(({ role, content }) => ({
-    role,
-    parts: [{ text: content }],
-  }));
-
-  return GM.startChat({ history });
-};
 
 app.post("/chat", async (req, res) => {
   try {
